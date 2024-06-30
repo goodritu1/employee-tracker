24f76019163f6c4444875728b601ebc7d8b47d91
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-06-29 23:07:13 -0700

    entered data to tables to pre-populate the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,24 @@
-INSERT INTO department (name) VALUES ('Sales');
-INSERT INTO department (name) VALUES ('Marketing');
-INSERT INTO department (name) VALUES ('Engineering');
+INSERT INTO department (name) 
+VALUES ('Sales'),
+ ('Marketing'),
+ ('Engineering');
 
-INSERT INTO role (title, salary, department_id) VALUES ('Salesperson', 50000, (SELECT id FROM department WHERE name = 'Sales'));
-INSERT INTO role (title, salary, department_id) VALUES ('Marketing Manager', 70000, (SELECT id FROM department WHERE name = 'Marketing'));
-INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 80000, (SELECT id FROM department WHERE name = 'Engineering'));
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Salesperson', 50000, 1),
+('Marketing Manager', 70000,2),
+('Software Engineer', 80000,3),
+('Accountant', 60000, 1),
+('Lawyer', 90000, 3),
+('Marketing Associate', 100000, 2),
+('HR Manager', 70000, 3),
+('Account Manager', 80000, 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', (SELECT id FROM
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL),
+('Ritu', 'Gupta', 2, 1),
+('Mike', 'Chan', 3, 1),
+('Tom', 'Allen', 4, 2),
+('Kevin', 'Malone', 5, 4),
+('Sarah', 'Lourd', 6, 1);
+
